a933c2c61d4dfe7a68d9496b8d13a0727f575d6a
eliabbaspk <afactoryshop2016@gmail.com>
2025-07-14 14:40:28 +0500

    signal_engine.py

--- core/signal_engine.py
+++ core/signal_engine.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 
 def get_live_signals():
-    return pd.DataFrame([
+    data = [
         {
             "Time": datetime.now().strftime("%H:%M:%S"),
             "Pair": "BTCUSDT",
@@ -12,4 +12,5 @@ def get_live_signals():
             "Take Profit": 58425.75,
             "Success Rate": "87.5%",
         }
-    ])
+    ]
+    return pd.DataFrame(data)
